8baf3ec169a89f2ff2e6be9e287a990d63ccf0b2
... <209888010@qq.com>
2025-07-29 21:38:28 +0800

    chore: 去除无关代码

--- src/markdown.ts
+++ src/markdown.ts
@@ -4,8 +4,6 @@ import { parseDocument } from 'htmlparser2'
 import { transformSync } from '@babel/core'
 import render from 'dom-serializer'
 import { TransformResult } from 'vite'
-import parse from 'html-react-parser'
-import { transformCodeTags } from './utils'
 
 export const createMarkdownToReact = (options: ResolvedOptions) => {
   const { wrapperDiv, wrapperComponent, wrapperClasses } = options
@@ -30,13 +28,7 @@ export const createMarkdownToReact = (options: ResolvedOptions) => {
     const markdownIt = await setupMarkdownItPromise
 
     const grayMatterFile = matter(raw)
-    // console.log(
-    //   '%c [ grayMatterFile ]',
-    //   'font-size:13px; background:pink; color:#bf2c9f;',
-    //   grayMatterFile,
-    // )
     let html = await markdownIt.renderAsync(grayMatterFile.content || '')
-    // html = html.replace(/<code(.*?)>/g, '<code$1 v-pre>')
 
     const parsedDocument = parseDocument(html, {
       lowerCaseTags: false,
@@ -44,22 +36,7 @@ export const createMarkdownToReact = (options: ResolvedOptions) => {
     })
     html = render(parsedDocument, { selfClosingTags: true, xmlMode: true })
 
-    // transformCodeTags(parsedDocument)
-
-    console.log(
-      '%c [ html ]',
-      'font-size:13px; background:pink; color:#bf2c9f;',
-      html,
-    )
-
-    // html = `<>${html}</>`
-
     html = `<div dangerouslySetInnerHTML={{__html: \`${html}\`}}></div>`
-    console.log(
-      '%c [ hddddddtml ]',
-      'font-size:13px; background:pink; color:#bf2c9f;',
-      html,
-    )
 
     if (wrapperDiv) {
       const resolvedWrapperClasses =
@@ -93,22 +70,6 @@ export const createMarkdownToReact = (options: ResolvedOptions) => {
         ['@babel/preset-react', { runtime: options.babelConfig.runtime }],
       ],
     })
-    console.log(
-      '%c [ renderStr ]',
-      'font-size:13px; background:pink; color:#bf2c9f;',
-      transformResult,
-    )
-
-    // return {
-    //   code: `
-    //   import React from 'react'
-    //   export default function () {
-    //     const frontmatter = ${grayMatterFile.data ? JSON.stringify(grayMatterFile.data) : '{}'}
-    //     return <div dangerouslySetInnerHTML={{__html:${transformResult?.code}}}></div>
-    //     }
-    //     `,
-    //   map: null,
-    // }
 
     const code = `
       import React from 'react'
@@ -117,11 +78,6 @@ export const createMarkdownToReact = (options: ResolvedOptions) => {
         return ${transformResult?.code || ''}
         }
         `
-    console.log(
-      '%c [ code ]',
-      'font-size:13px; background:pink; color:#bf2c9f;',
-      code,
-    )
 
     return {
       code,
--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,4 @@
 import { Options, ResolvedOptions } from './type'
-import type {
-  AnyNode,
-  Node as DomNode,
-  Element as DomElement,
-  Document,
-} from 'domhandler'
-import serialize from 'dom-serializer'
-import { getInnerHTML } from 'domutils'
 
 const includeRE = /\.md$/
 const cssIdRE = /\.(css|postcss|sass|scss|less|stylus|styl)($|\?)/
@@ -30,75 +22,4 @@ export const resolveOptions = (options: Options): ResolvedOptions => {
     ...defaultOptions,
     ...options,
   }
-}
-
-// export const handleCode = (node: DomHandlerNode) => {
-//   console.log(node, Object.keys(node))
-//   if (node instanceof Element) {
-//     // transformAttribs(node.attribs)
-//     if (node.tagName === 'code') {
-//       const codeContent = serialize(node.children)
-//       node.attribs['dangerouslySetInnerHTML'] =
-//         `vfm{{ __html: \`${escapeForTemplate(codeContent)}\`}}vfm`
-//       node.childNodes = []
-//     }
-//     // 递归处理子节点
-//     if (node.children && node.children.length > 0) {
-//       for (const child of node.children) {
-//         handleCode(child)
-//       }
-//     }
-//   }
-// }
-
-/**
- * 对模板字符串中的特殊字符进行转义
- */
-function escapeForTemplate(str: string): string {
-  return str
-    .replace(/\\/g, '\\\\') // 先转义反斜杠
-    .replace(/`/g, '\\`') // 再转义反引号
-    .replace(/\$/g, '\\$') // 防止 $ 被模板字面量误解
-}
-
-/**
- * 转义 HTML 属性值中的特殊字符
- */
-function escapeHTML(str: string): string {
-  return str
-    .replace(/&/g, '&amp;')
-    .replace(/"/g, '&quot;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-}
-
-/**
- * 处理 HTML 字符串中 <code> 元素，把内容提到 data-dangerously-html 上
- */
-export function transformCodeTags(document: Document): void {
-  function walk(node: DomNode): void {
-    if (node.type === 'tag') {
-      const el = node as DomElement
-
-      if (el.name === 'code') {
-        const inner = getInnerHTML(el, { decodeEntities: true })
-        el.attribs ||= {}
-        el.attribs['data-dangerously-html'] = escapeHTML(inner)
-        el.children = [] // 清空原来的 code 内容
-      }
-
-      // 遍历子节点
-      if (el.children?.length) {
-        for (const child of el.children) {
-          walk(child)
-        }
-      }
-    }
-  }
-
-  for (const child of Array.from(document.children)) {
-    walk(child as unknown as DomNode)
-  }
-
-  // return serialize(document);
 }
